24cad5129a6eb306528446d37b2dbb714920390c
Tuấn David <43137730+VitStudio@users.noreply.github.com>
2024-02-24 19:48:19 +0700

    First Update

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <style>
+        p,
+        dt {
+            font-weight: bold;
+        }
+
+        dl,
+        dd {
+            font-size: 1rem;
+        }
+
+        dd {
+            margin-bottom: 1em;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Welcome to My Website</h1>
+    <main>
+        <p>Score Per Test</p>
+
+        <dl>
+            <dt>Beast of Bodmin</dt>
+            <dd>A large feline inhabiting Bodmin Moor.</dd>
+
+            <dt>Morgawr</dt>
+            <dd>A sea serpent.</dd>
+
+            <dt>Owlman</dt>
+            <dd>A giant owl-like creature.</dd>
+        </dl>
+
+    </main>
+
+    <script src="index.js"></script>
+</body>
+
+</html>
---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'vitstudi_examget');
+define('DB_PASSWORD', 'davidbiskyhost@2024');
+define('DB_NAME', 'vitstudi_examget');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
